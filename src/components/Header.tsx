bdfa06761ec071c1d2adb3ee448ef6a7eb326724
TheNinjan <theninjan.com@gmail.com>
2023-09-09 12:01:28 +0530

    initial commit with minimum bare project

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <div className='w-full h-12 bg-flipkart_blue '>
+      <div>
+        Imag
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,26 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+      },
+      colors: {
+        flipkart_blue: "#2874f0",
+        flipkart_light: "#ffffff",
+        flipkart_yellow: "#ff9f00",
+        flipkart_orange: "#fb641b",
+        lightText: "#ccc",
+      },
+      fontFamily: {
+        bodyFont: ["Poppins", "sans-serif"],
+      },
     },
   },
   plugins: [],
